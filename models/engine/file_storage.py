5b122bf5ed53d0cf8c4ee8ec6d184f7225fd5a97
L-nsamba <l.nsamba@alustudent.com>
2026-01-09 17:37:22 +0200

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,60 +1,56 @@
 #!/usr/bin/python3
-"""
-FileStorage class
-Serializes instances to a JSON file and deserializes back to instances
-"""
-
+"""This module defines the FileStorage class for serialization."""
 import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
 
 
 class FileStorage:
+    """Serializes instances to a JSON file and deserializes back."""
     __file_path = "file.json"
     __objects = {}
 
+    # Map class names to classes for reload
+    classes = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Place": Place,
+        "Amenity": Amenity,
+        "Review": Review
+    }
+
     def all(self):
-        """Returns the dictionary __objects"""
-        return self.__objects
+        """Return the dictionary __objects"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        """Set in __objects the obj with key <obj class name>.id"""
+        if obj is None:
+            raise AttributeError("obj cannot be None")
+        FileStorage.__objects[f"{type(obj).__name__}.{obj.id}"] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file"""
-        obj_dict = {}
-        for key, obj in self.__objects.items():
-            obj_dict[key] = obj.to_dict()
-
-        with open(self.__file_path, "w") as f:
+        """Serialize __objects to the JSON file"""
+        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             json.dump(obj_dict, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
+        """Deserialize the JSON file to __objects, if file exists"""
         try:
-            with open(self.__file_path, "r") as f:
-                data = json.load(f)
-
-            classes = {
-                "BaseModel": BaseModel,
-                "User": User,
-                "State": State,
-                "City": City,
-                "Amenity": Amenity,
-                "Place": Place,
-                "Review": Review
-            }
-
-            for key, value in data.items():
-                cls_name = value["__class__"]
-                self.__objects[key] = classes[cls_name](**value)
-
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                obj_dict = json.load(f)
+            for key, val in obj_dict.items():
+                cls_name = val["__class__"]
+                cls = FileStorage.classes.get(cls_name)
+                if cls:
+                    FileStorage.__objects[key] = cls(**val)
         except FileNotFoundError:
             pass
